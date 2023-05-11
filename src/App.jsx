5096719497361a5bb16e94502195a46419fb783a
jollyfitrah93 <jollyfitrahbilitoni@gmail.com>
2023-05-11 16:33:42 +0700

    penyesuaian

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f2f2f2'  }}>
-      <h1 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '5px', color: '#333', marginBottom: '20px'}}>Portofolio Page</h1>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold', color: '#333', marginBottom: '20px'}}>Portofolio Page</h1>
       <nav style={{marginTop: '10px'}}>
         <button style={{marginRight: '25px', backgroundColor: '#3498db', border: 'none', color: 'white', padding: '10px 20px', textAlign: 'center', textDecoration:'none', display: 'inline-block', fontSize: '16px',borderRadius: '5px', cursor: 'pointer', boxShadow: '0 4px 6px rgba(0,0,0, 0.1)', transition: 'all 0.3s ease-in-out'}} onClick={() => handleNavigation('home')}>Home</button>
         <button style={{marginRight: '25px', backgroundColor: '#3498db', border: 'none', color: 'white', padding: '10px 20px', textAlign: 'center', textDecoration:'none', display: 'inline-block', fontSize: '16px',borderRadius: '5px', cursor: 'pointer', boxShadow: '0 4px 6px rgba(0,0,0, 0.1)', transition: 'all 0.3s ease-in-out'}} onClick={() => handleNavigation('portfolio')}>Portfolio</button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 
-function Home() {
+function home() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', padding: '20px', backgroundColor: '#f2f2f2' }}>
       <h2 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '10px', color: '#333' }}>Welcome, My name is Jolly Fitrah Bilitoni</h2>
       <p style={{ fontSize: '1.2rem', marginBottom: '20px', color: '#666' }}>Saya adalah team leader GraPARI Belitung.</p>
-      <img style={{ width: '100%', height: 'auto', maxWidth: '400px', borderRadius: '10px' }} src="./src/assets/Jolly Fitrah Bilitoni.jpeg" alt="random" />
+      <img style={{ width: '100%', height: 'auto', maxWidth: '400px', borderRadius: '10px' }} src="./src/assets/Jolly Fitrah Bilitoni.jpeg" alt="foto Jolly" />
     </div>
   );
 }
 
-export default Home;
+export default home;
